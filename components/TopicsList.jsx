b9086df963fe8aa9c120cdabd967e0c46c922926
anon-pradip <pradipsubedi360@gmail.com>
2023-06-24 06:56:05 +0545

    fetch all topics from db

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 
-const TopicsList = () => {
-  return (
-    <div className="border-slate-600 bg-slate-800 text-white border p-2 flex justify-between items-start mb-4">
-      <div>
-        <p className="text-xl font-bold">Topic Title</p>
-        <p>topic Description</p>
-      </div>
+const getTopics = async () => {
+  const response = await fetch("http://localhost:3000/api/topics");
+  const data = await response.json();
+  return data;
+};
 
-      <div className=" flex gap-2">
-        <RemoveBtn />
-        <Link href={"/editTopic/123"}>✏️</Link>
-      </div>
-    </div>
+const TopicsList = async () => {
+  const topics = await getTopics();
+  return (
+    <>
+      {topics.map((t) => (
+        <div className="border-slate-600 bg-slate-800 text-white border p-2 flex justify-between items-start mb-4">
+          <div>
+            <p className="text-xl font-bold">{t.title}</p>
+            <p>{t.description}</p>
+          </div>
+
+          <div className=" flex gap-2">
+            <RemoveBtn />
+            <Link href={`/editTopic/${t._id}`}>✏️</Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
